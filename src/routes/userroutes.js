e675ac58fe1e8f6ca79ab39c2963f87fd90e5ab0
kagabojaphet <iradukundajaphet@gmail.com>
2023-10-17 12:10:25 +0200

    add

--- src/routes/userroutes.js
+++ src/routes/userroutes.js
@@ -14,8 +14,8 @@ Validator.inputValidator,
 usercontroller.createruser)
 router.get("/",verifyaccess("admin"),usercontroller.getalluser)
 router.get("/:id",usercontroller.getOneUser)
-router.delete("/",verifyaccess("user"),usercontroller.deletealluser)
-router.delete("/:id",verifyaccess("user"),usercontroller.deleteOneUser)
-router.patch("/:id",verifyaccess("user"),usercontroller.updateUser)
+router.delete("/",verifyaccess("admin"),usercontroller.deletealluser)
+router.delete("/:id",verifyaccess("admin"),usercontroller.deleteOneUser)
+router.patch("/:id",verifyaccess("admin"),usercontroller.updateUser)
 router.post("/login",usercontroller.loginuser)
 export default router
