1472f3bba3e62b5a137306805b83c373fe402eef
kagabojaphet <iradukundajaphet59@gmail.com>
2023-10-23 16:26:05 +0200

    adding

--- src/routes/categoryroutes.js
+++ src/routes/categoryroutes.js
@@ -1,10 +1,11 @@
 import  express  from "express";
 import categorycontroller from "../controller/categorycontroller";
+import verifyaccess from "../middlewares/verifyaccess";
 
 const router=express.Router()
 
-router.post("/",categorycontroller.createcategory)
-router.get("/",categorycontroller.getallcategory)
+router.post("/",verifyaccess("admin"),categorycontroller.createcategory)
+router.get("/",verifyaccess("admin"),categorycontroller.getallcategory)
 
 
 export default router
